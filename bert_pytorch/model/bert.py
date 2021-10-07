dfdfcd50cd134e0ee2627a4c020b59b230d92995
林諦南 <Lindinan934301@gmail.com>
2021-10-07 20:35:44 +0800

    Add AL component.

--- bert_pytorch/model/bert.py
+++ bert_pytorch/model/bert.py
@@ -73,6 +73,7 @@ class BERTAL(nn.Module):
         # embedding for BERT, sum of positional, segment, token embeddings
         self.embedding = BERTEmbedding_AL(vocab_size=vocab_size, embed_size=hidden)
 
+        # TODO: switch to ALBlock, and remove ModuleList if it doesn't work.
         # multi-layers transformer blocks, deep network
         self.transformer_blocks = nn.ModuleList(
             [TransformerBlock(hidden, attn_heads, hidden * 4, dropout) for _ in range(n_layers)])
